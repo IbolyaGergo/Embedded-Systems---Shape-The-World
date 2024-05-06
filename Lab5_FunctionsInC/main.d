7788f163e288fcf2980509207303eaecd45d3995
IbolyaGergo <55050183+IbolyaGergo@users.noreply.github.com>
2024-05-06 11:46:17 +0200

    Add files via upload

---
+++ Lab5_FunctionsInC/main.d
@@ -0,0 +1,4 @@
+.\main.o: main.c
+.\main.o: C:\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdio.h
+.\main.o: uart.h
+.\main.o: TExaS.h
---
+++ Lab5_FunctionsInC/uart.d
@@ -0,0 +1,3 @@
+.\uart.o: UART.c
+.\uart.o: UART.h
+.\uart.o: C:\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdio.h
